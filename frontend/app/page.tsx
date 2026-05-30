433a7c862475e4f8df43a314cd50a29133797485
Debpratim Sharma <116699235+DebpratimSharma@users.noreply.github.com>
2026-05-31 02:49:48 +0530

    Remove LegalInsightsSection from page component
    Removed LegalInsightsSection from the main page.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,14 @@
 import { HeroSection } from "@/components/home/HeroSection";
 import { ProblemSection } from "@/components/home/ProblemSection";
 import { WorkspaceSection } from "@/components/home/WorkspaceSection";
-import { LegalInsightsSection } from "@/components/home/LegalInsightsSection";
+
 
   return (
     <div className="bg-background text-on-background min-h-screen antialiased w-full">
       <HeroSection />
       <ProblemSection />
       <WorkspaceSection />
-      <LegalInsightsSection />
+
     </div>
   );
 }
